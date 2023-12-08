4d7da2df67a2a708cb4d76b1b55c175abd648fe1
ArmanManasyan <arman.manasyan1@tumo.org>
2023-12-08 18:43:37 +0400

    LEsson 4>>

--- GameOfLifeArman/eventF/script.js
+++ GameOfLifeArman/eventF/script.js
@@ -1,14 +1,14 @@
-// let clickCount = 0;
-// function clickHandler(evt){
-// clickCount++;
-// console.log(evt);
-// let str = "Thanks for clicking " + clickCount;
-// this.innerText = str;
-// }
+let clickCount = 0;
+function clickHandler(evt){
+clickCount++;
+console.log(evt);
+let str = "Thanks for clicking " + clickCount;
+this.innerText = str;
+}
 
-// let  p = document.getElementById("pElement");
-// p.addEventListener("mouseover",clickHandler);
-// p.addEventListener("click",clickHandler);
+let  p = document.getElementById("pElement");
+p.addEventListener("mouseover",clickHandler);
+p.addEventListener("click",clickHandler);
 
 
 function bodyClick(evt){
